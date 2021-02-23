57e4bbc860906e011a10c1dd494ceab6a27dd7a5
Uzik7252 <79507509+Uzik7252@users.noreply.github.com>
2021-02-23 15:28:27 +0100

    przesyłam kod

---
+++ Pierwszy program/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        //zadanie 1
+        int A = 77;
+        double B = 7.7;
+        char C = '!';
+        String D = "Józef";
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+        //zadanie 2
+        int A2 = 5;
+        int B2 = 6;
+        int wynik = A2*A2 + B2*B2 ;
+        System.out.println("Wynik = " + wynik);
+
+        //zadanie 3
+        int A3 = 7;
+        double pole = A3*A3 * Math.sqrt(3) / 4;
+        System.out.println("Pole  trójkąta równobocznego o boku 7 wynosi = " + pole);
+
+        //zadanie 4
+        String imie = "Józef";
+        String nazwisko = " Rybakowski";
+        System.out.println(imie +  nazwisko);
+    }
+
+
+
+}
