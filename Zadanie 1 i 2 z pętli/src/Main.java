245a4aa9a81345ac704672918d13e16f79e7c95b
Uzik7252 <79507509+Uzik7252@users.noreply.github.com>
2021-02-23 19:22:02 +0100

    dodaje zadanie 1 i 2 z pętli

---
+++ Zadanie 1 i 2 z pętli/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Zadanie 1
+        char Litera = 'A';
+
+        for (int i = 0; i < 26; i++)
+        {
+            if ( i % 2 == 0)
+                System.out.print(Litera + "\t");
+            Litera++;
+        }
+        //Zadanie 2
+        //nie mam pojęcia jak to zrobić
+
+
+        }
+    }
+
+
